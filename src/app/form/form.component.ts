2d04f4951d54ab9ca796f794b1db564f627bc21b
rohitjaspal <jaspalrohitpanchkula@gmail.com>
2020-06-04 03:26:45 -0700

    saved data in localstorage gets decrypted

--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators, FormControl } from '@angular/forms';
 import * as CryptoJS from 'crypto-js';
-var CryptoJS = require("crypto-js");
+
 
 @Component({
   selector: 'app-form',
@@ -12,36 +12,40 @@ export class FormComponent implements OnInit {
   registerForm: FormGroup;
   submitted = false;
   savedvalue: any;
-  dataarray:any = [];
-  getlocalstoragedata:string[];
+  dataarray: any = [];
+  getlocalstoragedata: string[];
   displaydata: string;
+  ciphertext: any;
+  id: any;
+  tempdata: any;
+  allData: Storage;
+  key = 'secret key 123';
 
-  constructor(private formBuilder: FormBuilder) {}
+  constructor(private formBuilder: FormBuilder) { }
 
   ngOnInit() {
     this.registerForm = this.formBuilder.group({
-      fullName :  ['', Validators.required],
-      meetingDate: ['' , Validators.required],
-      meetingStartTime: ['' , Validators.required],
-      meetingEndTime : ['' , Validators.required]
+      fullName: ['', Validators.required],
+      meetingDate: ['', Validators.required],
+      meetingStartTime: ['', Validators.required],
+      meetingEndTime: ['', Validators.required]
     });
   }
-  
-get f() {
-       return this.registerForm.controls;
-      }
-      encryptdata(newname ,newmeetingDate,newmeetingStartTime ,newmeetingEndTime){
-        let key = 'key';
-        this.savedvalue = CryptoJS.AES.encrypt(newname.trim(),
-        newmeetingStartTime.trim(),
-        newmeetingEndTime.trim(),
-        newmeetingDate.trim(),key).toString();
-        console.log(this.savedvalue);
-        this.dataarray.push(this.savedvalue);
-        localStorage.setItem('data',JSON.stringify(this.dataarray));
-        console.log(this.dataarray);
-      }
-onSubmit() {
+
+  get f() {
+    return this.registerForm.controls;
+  }
+  encryptdata(newname, newmeetingDate, newmeetingStartTime, newmeetingEndTime) {
+    let key = 'key';
+    this.savedvalue = CryptoJS.AES.encrypt(newname.trim(),
+      newmeetingStartTime.trim(),
+      newmeetingEndTime.trim(),
+      newmeetingDate.trim(), key).toString();
+    console.log(this.savedvalue);
+    this.dataarray.push(this.savedvalue);
+    console.log(this.dataarray);
+  }
+  onSubmit() {
     this.submitted = true;
     if (this.registerForm.invalid) {
       return;
@@ -52,8 +56,21 @@ onSubmit() {
     let newmeetingDate = this.registerForm.value.meetingDate;
     let newmeetingStartTime = this.registerForm.value.meetingStartTime;
     let newmeetingEndTime = this.registerForm.value.meetingEndTime;
-    this.encryptdata(newname ,newmeetingDate,newmeetingStartTime ,newmeetingEndTime);
-    //this.clear();
+    const data = {
+      newname: newname,
+      newmeetingDate: newmeetingDate,
+      newmeetingStartTime: newmeetingStartTime,
+      newmeetingEndTime: newmeetingEndTime
+    }
+
+    this.ciphertext = CryptoJS.AES.encrypt(JSON.stringify(data), this.key).toString();
+    const i = this.rnd(10, 4000);
+    const id = 'id' + i;
+    this.id = id;
+    localStorage.setItem(this.id, this.ciphertext);
+  }
+  rnd(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) + min);
   }
   clear() {
     this.registerForm.value.fullName = '';
@@ -61,13 +78,17 @@ onSubmit() {
     this.registerForm.value.meetingEndTime = '';
     this.registerForm.value.meetingStartTime = '';
   }
-  decryptdata(){
-    let key = 'key'
-    this.displaydata = JSON.parse(localStorage.getItem('data'));
-    console.log(typeof this.displaydata[0]);
-    let rawvalue = 'U2FsdGVkX1+tAM1i1tukL1ckuk1i0t78zTuH599QJcc=';
-    //let rawvalue =this.displaydata[0];
-    this.getlocalstoragedata = CryptoJS.AES.decrypt(rawvalue,key).words.toString(CryptoJS.enc.Utf8);
-    console.log(this.getlocalstoragedata);
+
+
+  decryptdata() {
+    for (let i = 0; i < localStorage.length; i++) {
+      this.tempdata = localStorage.key(i);
+      this.ciphertext = localStorage.getItem(this.tempdata);
+      const bytes = CryptoJS.AES.decrypt(this.ciphertext, this.key);
+      const decryptedData = JSON.parse(bytes.toString(CryptoJS.enc.Utf8));
+
+      console.log("Decrypted data is", decryptedData);
+    }
+
   }
 }
